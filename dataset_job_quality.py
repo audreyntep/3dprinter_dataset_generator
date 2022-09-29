9332dfad9232ba0e4e47f3c5e03bbc40aab38152
Audrey <audrey.ntep@hotmail.fr>
2022-09-29 10:28:31 +0200

    fix: creation dataset job quality

--- dataset_job_quality.py
+++ dataset_job_quality.py
@@ -28,10 +28,8 @@ class PrintingJobFeatures():
         # creation des dimensions du produit (height, width, depth, layers)
         self.product_size()
         # creation de la liste des périmètres des layers
-        #self.set_layers_perimeters()
         self.set_linear_perimeter_and_speed()
         self.average_zaxis_speed()
-        self.set_material_used()
         self.average_uv_temperature()
         self.create_row()
 
@@ -55,25 +53,31 @@ class PrintingJobFeatures():
     def set_linear_perimeter_and_speed(self):
         speeds = []
         sum_speed = 0
+        gr_per_layer = random.uniform(0.8,2.3) # gr
+
         layers_perimeters = self.set_layers_perimeters()
-        for layer_perimeter in layers_perimeters: 
+
+        for layer_perimeter in layers_perimeters:
+            # sets total perimeter
             self.linear_perimeter += layer_perimeter
+            # sets speeds per layer
             speeds.append(round(layer_perimeter/self.linear_speed)) # mm/sec
-            self.elapsed_time += round(layer_perimeter/self.linear_speed)
+            # sets total time spent
+            deceleration_coeff = random.randint(4,10)/10 # printer head changing axes between 4 and 10 times
+            self.elapsed_time += round(layer_perimeter/(self.linear_speed*deceleration_coeff))
+        
+        # sets total material used
+        self.material_used = round(self.linear_perimeter*gr_per_layer)
+
         for speed in speeds:
             sum_speed += speed
         self.average_speed = sum_speed/len(speeds)
         
     def average_zaxis_speed(self):
         self.average_zaxis_speed = round(self.height/self.average_speed)
-            
-
-    def set_material_used(self):
-        gr_per_layer = random.randint(1,2) # gr
-        self.material_used = self.layers*gr_per_layer
 
     def average_uv_temperature(self):
-        self.uv_temperature = random.randint(70,120) # celsius
+        self.uv_temperature = random.randint(70,130) # celsius
 
     def create_row(self):
         row = {}
@@ -89,27 +93,25 @@ class PrintingJobFeatures():
 
 
 def create_rows():
-    dataset_max_size = 6000
-    start = 1600779498
 
-    total_elapsed_time = 0
+    dataset_max_size = 5000
+    start = 1600779498
     total_browsed_perimeter = 0
     total_material_used = 0
     total_uv_exposed = 0
 
-    theorical_time_deprecation = (3000*60*60)
-    theorical_perimeter_deprecation = 400000
-    theorical_material_deprecation = (19*100*1000)
-    uv_over_exposed = 1000
-
     rows = []
     keys = []
-    coeff_deprecation = 0.99
+    coeff_deprecation = 0.97
+    t,p,m = 0,0,0
 
     for i in range(1,dataset_max_size+1):
+
+        # creation des lignes
         row = PrintingJobFeatures().create_row()
         row.update({'index':i})
 
+        # insertion des valeurs timestamp et datetime
         if i == 1 :
             row['timestamp'] = start
         else :
@@ -117,31 +119,36 @@ def create_rows():
             start = row['timestamp']
         row['datetime'] = datetime.fromtimestamp(row['timestamp'])
 
+        uv_temperature = row['uv_temperature']
+        uv_temperature_threshold = 99 # degrees
+        if uv_temperature >= uv_temperature_threshold :
+            coeff_deprecation = 1-(uv_temperature/1000)
+            quality = round(row['quality']*coeff_deprecation) 
+            row.update({'quality': quality})
+            t += 1
+        
+        perimeter = row['perimeter']
+        perimeter_threshold = ((100*2 + 100*2)*round((100*10)/1.8))*10 # mm
+        if perimeter >= perimeter_threshold :
+            coeff_deprecation = (perimeter_threshold/perimeter)
+            quality = round(row['quality']*coeff_deprecation) 
+            row.update({'quality': quality})
+            p += 1
 
-        total_elapsed_time += row['elapsed_time']
-        total_browsed_perimeter += row['perimeter']
-        total_material_used += row['material_used']
-        if row['uv_temperature'] >= 100 :
-            total_uv_exposed += 1
+        material_used = row['material_used']
+        material_used_threshold = perimeter_threshold*2 # gr
+        if material_used >= material_used_threshold :
+            coeff_deprecation = material_used_threshold/material_used
+            quality = round(row['quality']*coeff_deprecation) 
+            row.update({'quality': quality})
+            m += 1
 
-        if total_elapsed_time >= theorical_time_deprecation or total_browsed_perimeter >= theorical_perimeter_deprecation :
-            if total_material_used >= theorical_material_deprecation or total_uv_exposed >= uv_over_exposed :
-                print(i, total_elapsed_time, total_browsed_perimeter, total_material_used, total_uv_exposed)
-                quality = round(row['quality']*coeff_deprecation) 
-                row.update({'quality': quality})
-                coeff_deprecation -= 0.01
-                if quality <= 97 :
-                    total_elapsed_time = 0
-                    total_browsed_perimeter = 0
-                    total_material_used = 0
-                    total_uv_exposed = 0
-                    coeff_deprecation = 0.99
         rows.append(row)
 
         if i == dataset_max_size-1 :
             keys = row.keys()
 
-        #pprint.pprint(rows)
+    print(t,p,m)
     return keys, rows
 
 
@@ -158,5 +165,5 @@ def create_csv(filename, columns, rows):
     print('csv file "%s" created'% filename)
 
 columns, rows = create_rows()
-filename = 'printing_dataset.csv'
+filename = 'dataset_job_quality.csv'
 create_csv(filename=filename,columns=columns, rows=rows)
